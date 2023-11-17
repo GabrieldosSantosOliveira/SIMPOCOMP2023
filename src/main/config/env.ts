4f4729183c0778cdc24e6cb0157bd788cc97d607
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-11-17 12:33:15 -0400

    fix: converted APP_PORT  to Number

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 export const env = {
-  APP_PORT: process.env.APP_PORT ?? 3333,
+  APP_PORT: Number(process.env.APP_PORT) || 3333,
   SECRET_ACCESS_TOKEN_KEY:
     process.env.SECRET_ACCESS_TOKEN_KEY ?? 'SECRET_ACCESS_TOKEN_KEY',
 }
